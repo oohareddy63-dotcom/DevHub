03609c966f33f1c0e51ccaa4b6b7ed72efafd685
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-06-14 18:25:15 +0530

    Update import statements in BuildLogFeed.js

--- frontend/components/BuildLogFeed.js
+++ frontend/components/BuildLogFeed.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchBuildLogs, createBuildLog } from '../store/slices/buildLogSlice';
 import { Plus, Filter, Code } from 'lucide-react';
 import BuildLogCard from './BuildLogCard';
+
 export default function BuildLogFeed() {
   const dispatch = useDispatch();
   const { buildLogs, isLoading, error } = useSelector((state) => state.buildLogs);
