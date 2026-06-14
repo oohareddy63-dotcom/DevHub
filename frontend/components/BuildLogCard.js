08bc562760e03331727f9a8d8d82f1acac023a28
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-06-14 18:24:59 +0530

    Add new icons to BuildLogCard component

--- frontend/components/BuildLogCard.js
+++ frontend/components/BuildLogCard.js
@@ -14,6 +14,7 @@ import {
 import {
     BookOpen, Code, TestTube, Rocket, AlertTriangle,
     Heart, MessageCircle, HelpCircle,
+    
     ChevronDown, ChevronUp, CheckCircle, XCircle,
     Calendar, TrendingUp, AlertOctagon, ThumbsUp, Check,
     AlertCircle
