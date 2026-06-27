5f62ce39fd13008ca56556c17c592b697ec158a7
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-06-27 22:02:40 +0530

    Remove unused import 'addComment' from BuildLogCard

--- frontend/components/BuildLogCard.js
+++ frontend/components/BuildLogCard.js
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
     likeBuildLog,
-    
     addComment,
     requestHelp,
     addProgressUpdate,
